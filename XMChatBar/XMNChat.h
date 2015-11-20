311519ab51ce8d081048f4a241d7ed4121fd6c67
XMFraker <3057600441@qq.com>
2015-11-20 10:15:09 +0800

    增加XMNMessage 所有定义

---
+++ XMChatBar/XMNChat.h
@@ -0,0 +1,27 @@
+//
+//  XMNChat.h
+//  XMNChatExample
+//
+//  Created by shscce on 15/11/16.
+//  Copyright © 2015年 xmfraker. All rights reserved.
+//
+
+#ifndef XMNChat_h
+#define XMNChat_h
+
+#import "XMChatBar.h"
+#import "XMNChatMessageCell.h"
+#import "XMNChatTextMessageCell.h"
+#import "XMNChatVoiceMessageCell.h"
+#import "XMNChatImageMessageCell.h"
+#import "XMNChatSystemMessageCell.h"
+#import "XMNChatLocationMessageCell.h"
+
+#import "XMNChatUntiles.h"
+
+#import "XMNChatViewModel.h"
+
+#import "XMNAVAudioPlayer.h"
+
+
+#endif /* XMNChat_h */
